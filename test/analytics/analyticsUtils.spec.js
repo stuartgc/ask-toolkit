4890217ad81d3d6310843ea878ab2eed9e9c07dc
Stuart Chandra <schandra2@hearst.com>
2018-02-15 15:35:46 -0800

    utils test

--- test/analytics/analyticsUtils.spec.js
+++ test/analytics/analyticsUtils.spec.js
@@ -3,35 +3,37 @@
 const analyticsHelper = require( "./../../lib/analytics/analyticsUtils" ),
     c = require( "./../../lib/analytics/constants" );
 
-describe( "analyticsUtils.isTrackableRequest ", function() {
-    it( "should return true for intent request", function() {
-        const event = {
-            session: {
-                user: {
-                    userId: "abc123"
+describe( "ANALYTICS UTILS", function() {
+    describe( "analyticsUtils.isTrackableRequest ", function() {
+        it( "should return true for intent request", function() {
+            const event = {
+                session: {
+                    user: {
+                        userId: "abc123"
+                    }
+                },
+                request: {
+                    type: c.requestType.intentRequest
                 }
-            },
-            request: {
-                type: c.requestType.intentRequest
             }
-        }
 
-        expect( analyticsHelper.isTrackableRequest( event ) ).to.be.true;
-    } );
+            expect( analyticsHelper.isTrackableRequest( event ) ).to.be.true;
+        } );
 
 
-    it( "should return false when user data is not available", function() {
-        const event = {
-            session: {
-                attributes: {
-                    STATE: "_DAILY_TIP"
+        it( "should return false when user data is not available", function() {
+            const event = {
+                session: {
+                    attributes: {
+                        STATE: "_DAILY_TIP"
+                    }
+                },
+                request: {
+                    type: "AudioPlayer.PlaybackStarted"
                 }
-            },
-            request: {
-                type: "AudioPlayer.PlaybackStarted"
             }
-        }
 
-        expect( analyticsHelper.isTrackableRequest( event ) ).to.be.false;
+            expect( analyticsHelper.isTrackableRequest( event ) ).to.be.false;
+        } );
     } );
 } );
