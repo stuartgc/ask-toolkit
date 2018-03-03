ee094dfa8d383709c521ad0bbd6ff4710517cbcd
Stuart Chandra <schandra2@hearst.com>
2018-03-02 16:21:56 -0800

    fix tests

--- test/response/responseHandlers.spec.js
+++ test/response/responseHandlers.spec.js
@@ -6,8 +6,6 @@ const c = require( "./../../lib/analytics/constants" ),
     responseHandlers = require( "./../../lib/response/responseHandlers" );
 
 describe( "RESPONSE HANDLERS", function() {
-    const callback = sinon.stub().returnsArg( 0 );
-
     beforeEach( function() {
         Object.assign( responseHandlers, {
             attributes: {},
@@ -43,34 +41,31 @@ describe( "RESPONSE HANDLERS", function() {
         } );
     } );
 
-    afterEach( function() {
-    } );
-
     describe( "responseHandlers[ \"::onBeforeResponse\" ]", function() {
         it( "Tracking with no extra methods", function( done ) {
-            const spy = sinon.spy( responseHandlers, "emit" );
+            sinon.stub( responseHandlers, "emit" ).callsFake( ( a ) => {
+                expect( a ).to.equal(  "::respond" );
+
+                done();
+            } );
 
             responseHandlers[ "::onBeforeResponse" ]( responseObj, null );
-
-            expect( spy.calledWith( "::respond" ) ).to.be.true;
-
-            done();
         } );
     } );
 
     describe( "responseHandlers[ \"::onBeforeResponse\" ]", function() {
         it( "Tracking with extra method", function( done ) {
-            const spy = sinon.spy( responseHandlers, "emit" );
+            sinon.stub( responseHandlers, "emit" ).callsFake( ( a ) => {
+                expect( a ).to.equal(  "::respond" );
+
+                done();
+            } );
 
             queue.addToBeforeResponse( function( cb ) {
                 cb();
             } );
 
             responseHandlers[ "::onBeforeResponse" ]( responseObj, null );
-
-            expect( spy.calledWith( "::respond" ) ).to.be.true;
-
-            done();
         } );
     } );
 } );
