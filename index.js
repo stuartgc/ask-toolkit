b53388859074ffb17fda28886f1b0719d03530bb
Stuart Chandra <schandra2@hearst.com>
2018-06-18 17:05:51 -0700

    commented out list

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 "use strict";
 
 const analytics = require( "./lib/analytics/analytics" ),
-    list = require( "./lib/list-api/list" ),
+    // list = require( "./lib/list-api/list" ),
     response = require( "./lib/response/response" ),
     utils = require( "./lib/utils/utils" );
 
+//TODO: fix list
 module.exports.analytics = analytics;
-module.exports.list = list;
+// module.exports.list = list;
 module.exports.response = response;
 module.exports.utils = utils;
