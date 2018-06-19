c5c1629664124609233b945cf8d4f0e5c248fd76
Stuart Chandra <schandra2@hearst.com>
2018-06-19 11:05:47 -0700

    persistence

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const analytics = require( "./lib/analytics/analytics" ),
     // list = require( "./lib/list-api/list" ),
     localization = require( "./lib/localization/localization" ),
+    persistence = require( "./lib/persistence/persistence" ),
     response = require( "./lib/response/response" ),
     utils = require( "./lib/utils/utils" );
 
@@ -10,5 +11,6 @@ const analytics = require( "./lib/analytics/analytics" ),
 module.exports.analytics = analytics;
 // module.exports.list = list;
 module.exports.localization = localization;
+module.exports.persistence = persistence;
 module.exports.response = response;
 module.exports.utils = utils;
