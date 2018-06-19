f911116fe4ddbcbc53be0df8d5d370546f902fee
Stuart Chandra <schandra2@hearst.com>
2018-06-19 10:57:21 -0700

    localization

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 
 const analytics = require( "./lib/analytics/analytics" ),
     // list = require( "./lib/list-api/list" ),
+    localization = require( "./lib/localization/localization" ),
     response = require( "./lib/response/response" ),
     utils = require( "./lib/utils/utils" );
 
 //TODO: fix list
 module.exports.analytics = analytics;
 // module.exports.list = list;
+module.exports.localization = localization;
 module.exports.response = response;
 module.exports.utils = utils;
