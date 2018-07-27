c0bdd8b0abc3857a9e01fedb07e79dd015c58a29
Stuart Chandra <schandra2@hearst.com>
2018-07-26 17:16:17 -0700

    updated list api for add and permissions

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 "use strict";
 
 const analytics = require( "./lib/analytics/analytics" ),
-    // list = require( "./lib/list-api/list" ),
+    listApi = require( "./lib/list-api/listApi" ),
     localization = require( "./lib/localization/localization" ),
     persistence = require( "./lib/persistence/persistence" ),
     response = require( "./lib/response/response" ),
     utils = require( "./lib/utils/utils" );
 
-//TODO: fix list
 module.exports.analytics = analytics;
-// module.exports.list = list;
+module.exports.listApi = listApi;
 module.exports.localization = localization;
 module.exports.persistence = persistence;
 module.exports.response = response;
